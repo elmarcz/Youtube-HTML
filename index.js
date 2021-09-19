c661fa4b1b2c1a2853cfe9a67073290713d9c4bd
Marc Medrano <83883889+elmarcz@users.noreply.github.com>
2021-09-19 19:55:26 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+
+    <link rel="icon" type="image/png" href="Imagenes/favicon.ico" sizes="32x32">
+
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="Css/head.css">
+    <link rel="stylesheet" href="Css/videos.css">
+    <link rel="stylesheet" href="Css/menú.css">
+    <link rel="stylesheet" href="Css/normalize.css">
+    <head>
+        <title>YouTube</title>
+    </head>
+    <body>
+        <div class="container">
+
+            <div class="navegación">
+                <div class="separador"></div>
+                <div class="interior">
+                    
+                <form action="" class="form-box">
+                    <input type="text" placeholder="Buscar" required class="barra">
+                    <input type="submit" class="btn">
+                </form> 
+                </div>
+
+
+                <div class="menú">
+                    
+                </div>
+            
+                     
+                </div>
+            </div>
+       
+            </div>
+
+            <div class="container-videos">
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            <a href="video.html">
+                <div class="tarjeta">
+                    <div src="Imagenes/FONDOS PARA TUS MINIATURAS/fondo miniatura6.jpg" class="miniatura"></div>
+                    <img src="Imagenes/Marc Medrano.png" class="logo-autor">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit</h4>
+                    <p>Lorem Ipsum</p>
+                    <p>5M de visualizaciones · hace 1 semana</p> 
+                </div>
+            </a>
+            </div>
+        </div>
+    </body>
+</html>
